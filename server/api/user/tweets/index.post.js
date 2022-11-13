d6b7004291b484081405eac7501d8bc508c48ba1
§ <1309346392@qq.com>
2022-11-13 12:32:27 +0800

    Add the clodinary url and public id in the mediafile

--- server/api/user/tweets/index.post.js
+++ server/api/user/tweets/index.post.js
@@ -2,6 +2,7 @@ import formidable from "formidable";
 import { createMediaFile } from "~~/server/db/mediaFiles";
 import { createTweet } from "~~/server/db/tweets";
 import { tweetTransformer } from "~~/server/transformers/tweet";
+import { uploadToCloudinary } from "~~/server/utils/cloudinary";
 
 export default defineEventHandler(async (event) => {
   const form = formidable({});
@@ -27,19 +28,24 @@ export default defineEventHandler(async (event) => {
 
   const tweet = await createTweet(tweetData);
 
-  const filePromises = Object.keys(files).map(async (key) =>
-    createMediaFile({
-      url: "",
-      providerPublicId: "random_id",
+  const filePromises = Object.keys(files).map(async (key) => {
+    const file = files[key];
+    const cloudinaryResource = await uploadToCloudinary(file.filepath);
+
+    console.log(cloudinaryResource);
+
+    return createMediaFile({
+      url: cloudinaryResource.secure_url,
+      providerPublicId: cloudinaryResource.public_id,
       userId: userId,
       tweetId: tweet.id,
-    })
-  );
+    });
+  });
 
   await Promise.all(filePromises);
 
   return {
     // hello: tweetTransformer(tweet),
-    files
+    files,
   };
 });
