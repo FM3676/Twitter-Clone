622c500d823b827ad156cb6b0b109e4717ee9e4f
§ <1309346392@qq.com>
2022-11-14 00:34:30 +0800

    Finish Button Component

---
+++ components/UI/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    class="flex justify-center text-white bg-blue-400 rounded-full hover:bg-blue-500 font-sm disabled:bg-blue-300 disabled:cursor-not-allowed"
+    :disabled="props.disabled"
+    :class="classes"
+  >
+    <span :class="textFontSize">
+      <slot />
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  disabled: { type: Boolean, default: false },
+  size: { type: String, default: "md" },
+  liquid: { type: Boolean, default: false },
+});
+
+const paddingClasses: string =
+  props.size === "sm"
+    ? "px-3 py-2"
+    : props.size === "lg"
+    ? "px-4 py-3"
+    : "py-3 px-3";
+
+const textFontSize: string = props.size === "lg" ? "text-md" : "text-sm";
+
+const classes: string = `${paddingClasses} ${
+  props.liquid ? "w-full" : "w-min"
+} `;
+</script>
+
+<style scoped></style>
