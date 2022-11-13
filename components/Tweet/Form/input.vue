89d50d1f119e2c20558e8ef6c66079fb0397dcb2
§ <1309346392@qq.com>
2022-11-13 14:50:04 +0800

    Finish basic file select input

--- components/Tweet/Form/input.vue
+++ components/Tweet/Form/input.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="flex items-center flex-shrink-0 p-4 pb-0">
+      <!-- Profile Image -->
       <div class="flex w-12 items-top">
         <img
           :src="props.user?.profileImage"
@@ -8,6 +9,7 @@
           class="inline-block w-10 h-10 rounded-full"
         />
       </div>
+      <!-- TextArea -->
       <div class="w-full p-2">
         <textarea
           v-model="text"
@@ -15,11 +17,40 @@
         ></textarea>
       </div>
     </div>
-    <div><button @click="handleFormSubmit">Tweet</button></div>
+
+    <!-- File Selector -->
+    <div class="p-4 pl-16">
+      <input
+        type="file"
+        hidden
+        ref="imageInput"
+        accept="image/png, image/gif, image/jpeg, image/jpg"
+        @change=""
+      />
+    </div>
+
+    <!-- Icons -->
+    <div class="flex p-2 pl-14">
+      <Image @click="handleImageClick" />
+      <Gif />
+      <Chart />
+      <Emoji />
+      <Calendar />
+    </div>
+    <div>
+      <button @click="handleFormSubmit">Tweet</button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {
+  Image,
+  Gif,
+  Chart,
+  Emoji,
+  Calendar,
+} from "~~/components/Logo/PostFormIconList";
 interface User {
   username: string;
   email: string;
@@ -31,8 +62,20 @@ const props = defineProps<{ user: User }>();
 const emits = defineEmits(["onSubmit"]);
 
 const text = ref<string>("");
+const imageInput = ref<HTMLInputElement>();
 
 const handleFormSubmit = (): void => emits("onSubmit", { text: text.value });
+
+const handleImageClick = (): void => imageInput.value.click();
+
+const handleImageChange = (): void => {};
 </script>
 
-<style scoped></style>
+<style scoped>
+.w-5 {
+  width: 1.25rem;
+}
+.h-5 {
+  height: 1.25rem;
+}
+</style>
