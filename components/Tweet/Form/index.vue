705fc8435ee8a3b7da3b021403716949f055f418
§ <1309346392@qq.com>
2022-11-13 17:34:04 +0800

    Add the loading animation after posting

--- components/Tweet/Form/index.vue
+++ components/Tweet/Form/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div><TweetFormInput :user="props.user" @on-submit="handleFormSubmit" /></div>
+  <div>
+    <div v-if="loading" class="flex items-center justify-center py-6">
+      <UISpinner />
+    </div>
+    <div v-else>
+      <TweetFormInput :user="props.user" @on-submit="handleFormSubmit" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -17,17 +24,22 @@ interface SubmitFormData {
 
 const props = defineProps<{ user: User }>();
 
+const loading = ref<boolean>(false);
+
 const { postTweet } = useTweets();
 
-const handleFormSubmit = async (data:SubmitFormData) => {
+const handleFormSubmit = async (data: SubmitFormData) => {
+  loading.value = true;
   try {
-      const response = await postTweet({
-          text: data.text,
-          mediaFiles:data.mdeiaFiles
+    const response = await postTweet({
+      text: data.text,
+      mediaFiles: data.mdeiaFiles,
     });
     console.log(response);
   } catch (error) {
     console.log(error);
+  } finally {
+    loading.value = false;
   }
 };
 </script>
