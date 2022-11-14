8827997d940c267f33648297899b09b587df61fa
§ <1309346392@qq.com>
2022-11-15 00:50:49 +0800

    Add the Head Title and Form border in homepage.

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <MainSection title="Content" :loading="loading">
-      <TweetForm :user="user" />
+    <MainSection title="Home" :loading="loading">
+      <Head>
+        <Title>Home/ Twitter</Title>
+      </Head>
+      <div class="border-b" :class="twitterBorderColor">
+      
+        <TweetForm :user="user" />
+      </div>
     </MainSection>
   </div>
 </template>
 
 <script setup lang="ts">
+const { twitterBorderColor } = useTailwindConfig();
 const loading = ref<boolean>(false);
 const { useAuthUser } = useAuth();
 const user = useAuthUser();
