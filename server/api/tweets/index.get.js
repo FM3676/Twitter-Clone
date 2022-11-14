c00fbcd658aa50baf84546321e80e094747e938f
§ <1309346392@qq.com>
2022-11-15 00:39:39 +0800

    Add get Tweets API

---
+++ server/api/tweets/index.get.js
@@ -0,0 +1,20 @@
+import { getTweets } from "~~/server/db/tweets";
+import { tweetTransformer } from "~~/server/transformers/tweet";
+
+export default defineEventHandler(async (event) => {
+  const tweets = await getTweets({
+    include: {
+      author: true,
+      mediaFiles: true,
+      replies: {
+        include: { author: true },
+      },
+      replyTo: { include: { author: true } },
+    },
+    orderBy: [{ createAt: "desc" }],
+  });
+
+  return {
+    tweets: tweets.map(tweetTransformer),
+  };
+});
