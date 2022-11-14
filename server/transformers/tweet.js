9f9eba8216ee482e6ae4e5205cab575592afd172
§ <1309346392@qq.com>
2022-11-15 00:40:32 +0800

    Upadate tweet Transformer

--- server/transformers/tweet.js
+++ server/transformers/tweet.js
@@ -1,1 +1,18 @@
-export const tweetTransformer = (tweet) => ({ id: tweet.id, text: tweet.text });
+import { mediaFileTransformer } from "./mediaFiles";
+import { userTransformer } from "./user";
+import human from "human-time";
+
+export const tweetTransformer = (tweet) => ({
+  id: tweet.id,
+  text: tweet.text,
+  mediaFiles: !!tweet.mediaFiles
+    ? tweet.mediaFiles.map(mediaFileTransformer)
+    : [],
+  author: !!tweet.author ? userTransformer(tweet.author) : null,
+  replies: !!tweet.replies ? tweet.replies.map(tweetTransformer) : [],
+  replyTo: !!tweet.replyTo ? tweetTransformer(tweet.replyTo) : null,
+  repliesCount: !!tweet.replies ? tweet.replies.length : 0,
+  postedAtHuman: human(
+    Math.abs(Date.parse(tweet.createAt) - Date.now()) / 1000
+  ),
+});
