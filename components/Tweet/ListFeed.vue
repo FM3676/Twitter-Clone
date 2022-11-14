7118dfe965c34c0737b2d852a3aeb11f6e46f2e4
§ <1309346392@qq.com>
2022-11-15 01:16:21 +0800

    Init ListFeed

---
+++ components/Tweet/ListFeed.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>list feed</h1>
+    <ul>
+      <li v-for="tweet in props.tweets">{{ tweet.text }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ tweets: Tweet[] }>();
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,32 @@
         <Title>Home/ Twitter</Title>
       </Head>
       <div class="border-b" :class="twitterBorderColor">
-      
         <TweetForm :user="user" />
       </div>
+
+      <TweetListFeed :tweets="homeTweets" />
     </MainSection>
   </div>
 </template>
 
 <script setup lang="ts">
 const { twitterBorderColor } = useTailwindConfig();
-const loading = ref<boolean>(false);
 const { useAuthUser } = useAuth();
+const { getHomeTweets } = useTweets();
 const user = useAuthUser();
+const loading = ref<boolean>(false);
+const homeTweets = ref<Tweet[]>([]);
+
+onBeforeMount(async () => {
+  loading.value = true;
+  try {
+    const { tweets } = await getHomeTweets();
+    homeTweets.value = tweets as Tweet[];
+  } catch {
+  } finally {
+    loading.value = false;
+  }
+});
 </script>
 
 <style scoped></style>
