3e8a84b116f31f5ab500c4c4f0f005f1661fb2a8
§ <1309346392@qq.com>
2022-11-15 13:00:11 +0800

    Finish Tweet list item feed

--- components/Tweet/ListFeed.vue
+++ components/Tweet/ListFeed.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
-    <h1>list feed</h1>
-    <ul>
-      <li v-for="tweet in props.tweets">{{ tweet.text }}</li>
-    </ul>
+  <div v-if="isEmptyTweet" class="p-4">
+    <p class="text-center text-gray-500">No tweets</p>
   </div>
+  <ul>
+    <li
+      v-for="tweet in props.tweets"
+      :key="tweet.id"
+      class="pb-4 border-b cursor-pointer hover:bg-gray-100 dark:hover:bg-dim-300"
+      :class="[twitterBorderColor, defaultTransition]"
+    >
+      <TweetItem :tweet="tweet" />
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
+const { twitterBorderColor, defaultTransition } = useTailwindConfig();
 const props = defineProps<{ tweets: Tweet[] }>();
+const isEmptyTweet = props.tweets.length === 0;
 </script>
 
 <style scoped></style>
