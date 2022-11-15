c47a661cee1f8e25c792b22474433372af988658
§ <1309346392@qq.com>
2022-11-15 15:19:15 +0800

    Finish basic Tweet Detail page

---
+++ components/Tweet/Details.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TweetItem :tweet="props.tweet" />
+
+    <TweetForm :user="props.user" placeholder="Tweet your reply" />
+
+    <TweetListFeed :tweets="replies" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ user: User; tweet: Tweet }>();
+const replies = !!props.tweet.replies ? props.tweet.replies : [];
+</script>
+
+<style scoped></style>
